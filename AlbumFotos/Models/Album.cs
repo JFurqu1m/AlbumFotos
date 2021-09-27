fb827e695848c86876a0cbdac5dfbd56ce885ea8
Jeferson Furquim <jeferson.souza@cadis.com.br>
2021-09-27 14:43:59 -0300

    Criando Models e configurando conexao com Banco de dados

---
+++ AlbumFotos/Models/Album.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumFotos.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage="Campo obrigátorio")]
+        [StringLength(50, ErrorMessage ="Use menos caracteres")]
+        public string Destino { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigátorio")]
+        public string FotoTopo { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString ="{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "Campo obrigátorio")]
+        public DateTime Inicio { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "Campo obrigátorio")]
+        public DateTime Fim { get; set; }
+
+        public ICollection<Imagem> Imagens { get; set; }
+
+    }
+}
